580392a4694eef09ed2225c6dc7bbfd7f5263f7f
Johannes Mikulasch <johannes.mikulasch@zmp.co.jp>
2016-11-07 13:39:22 +0900

    source bashrc after bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ function sync_files() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
+	source ~/.bashrc;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
